a27df6ecbd78a3a53a9bc150d80445ffab993f87
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2018-03-01 18:39:52 +0900

    Refactor restaurant model

--- Roboter/models/Restaurant.py
+++ Roboter/models/Restaurant.py
@@ -1,5 +1,7 @@
+import collections
 import csv
 import os
+import pathlib
 
 DEFAULT_CSV_FILE_PATH = 'data.csv'
 
@@ -8,76 +10,44 @@ RESTAURANT_COLUMN_COUNT = 'COUNT'
 
 
 class Restaurant(object):
-    def __init__(self,
-                 csv_file_name=DEFAULT_CSV_FILE_PATH,
-                 column_name=RESTAURANT_COLUMN_NAME,
-                 column_count=RESTAURANT_COLUMN_COUNT):
-        self.csv_file_name = os.path.join(os.getcwd(), csv_file_name)
-        self.column_name = column_name
-        self.column_count = column_count
-        self.fieldnames = [self.column_name, self.column_count]
+    def __init__(self, csv_file=DEFAULT_CSV_FILE_PATH):
+        self.csv_file = os.path.join(os.getcwd(), csv_file)
+        self.columns = [RESTAURANT_COLUMN_NAME, RESTAURANT_COLUMN_COUNT]
+        self.data = collections.defaultdict(int)
 
-        if not self.is_exists_csv_file():
-            self.create_csv_file()
+        if not os.path.exists(self.csv_file):
+            pathlib.Path(self.csv_file).touch()
 
-    def is_exists_csv_file(self):
-        return os.path.exists(self.csv_file_name)
+        self.load_data()
 
-    def create_csv_file(self):
-        with open(self.csv_file_name, 'w') as cf:
-            writer = csv.DictWriter(cf, fieldnames=self.fieldnames)
+    def load_data(self):
+        with open(self.csv_file, 'r') as cf:
+            reader = csv.DictReader(cf)
+            for row in reader:
+                self.data[row[RESTAURANT_COLUMN_NAME]] = int(
+                    row[RESTAURANT_COLUMN_COUNT])
+        return self.data
+
+    def save(self):
+        with open(self.csv_file, 'w') as cf:
+            writer = csv.DictWriter(cf, fieldnames=self.columns)
             writer.writeheader()
-
-    def save_restaurant(self, restaurant_name):
-        if self.find_by_restaurant_name(restaurant_name):
-            self.increment_count(restaurant_name)
-        else:
-            with open(self.csv_file_name, 'a') as cf:
-                writer = csv.DictWriter(cf, fieldnames=self.fieldnames)
+            for k, v in self.data.items():
                 writer.writerow({
-                    self.column_name: restaurant_name,
-                    self.column_count: 1
+                    RESTAURANT_COLUMN_NAME: k,
+                    RESTAURANT_COLUMN_COUNT: v
                 })
 
-    def find_by_restaurant_name(self, restaurant_name):
-        with open(self.csv_file_name, 'r') as cf:
-            reader = csv.DictReader(cf)
-            for row in reader:
-                if row[self.column_name] == restaurant_name:
-                    return True
-                    break
+    def increment(self, name):
+        self.data[name.capitalize()] = self.data[name.capitalize()] + 1
+        self.save()
 
-    def increment_count(self, restaurant_name):
-        update_list = []
-        with open(self.csv_file_name, 'r+') as cf:
-            reader = csv.DictReader(cf)
-            for row in reader:
-                if row[self.column_name] == restaurant_name:
-                    d = {
-                        self.column_name: row[self.column_name],
-                        self.column_count: int(row[self.column_count]) + 1
-                    }
-                    update_list.append(d)
-                else:
-                    update_list.append(row)
-            cf.seek(0)
-            writer = csv.DictWriter(cf, fieldnames=self.fieldnames)
-            writer.writeheader()
-            writer.writerows(update_list)
+    def get_most_popular(self, exclude_list=None):
+        if exclude_list is None:
+            exclude_list = []
 
-    def get_top_restaurant_name(self):
-        top_restaurant = {}
-        with open(self.csv_file_name, 'r') as cf:
-            reader = csv.DictReader(cf)
-            for row in reader:
-                if top_restaurant:
-                    if top_restaurant[self.column_count] < row[self.
-                                                               column_count]:
-                        top_restaurant = dict(row)
-                else:
-                    top_restaurant = dict(row)
-
-        if top_restaurant:
-            return top_restaurant[self.column_name]
-        else:
-            return None
+        sorted_data_list = sorted(self.data, key=self.data.get, reverse=True)
+        for name in sorted_data_list:
+            if name in exclude_list:
+                continue
+            return name
--- Roboter/models/Robot.py
+++ Roboter/models/Robot.py
@@ -14,13 +14,13 @@ class Robot(object):
         self.user_name = user_name
 
     def recommend_restaurant(self):
-        top_restaurant_name = self.restaurant_model.get_top_restaurant_name()
-        if top_restaurant_name:
+        most_popluar_restaurant = self.restaurant_model.get_most_popular()
+        if most_popluar_restaurant:
             while True:
-                answer = input("私のオススメのレストランは、" + top_restaurant_name +
+                answer = input("私のオススメのレストランは、" + most_popluar_restaurant +
                                'です。\nこのレストランは好きですか？ [Yes/No]').lower()
                 if answer in ('yes', 'y'):
-                    self.restaurant_model.save_restaurant(top_restaurant_name)
+                    self.restaurant_model.increment(most_popluar_restaurant)
                     break
                 elif answer in ('no', 'n'):
                     break
@@ -29,7 +29,7 @@ class Robot(object):
 
     def ask_favorite_restaurant(self):
         favorite_restaurant = input(self.user_name + "さん。どこのレストランが好きですか？\n")
-        self.restaurant_model.save_restaurant(favorite_restaurant.capitalize())
+        self.restaurant_model.increment(favorite_restaurant)
 
     def say_good_bye(self):
         print(self.user_name + "さん。ありがとうございました。\n良い一日を！さようなら。")
